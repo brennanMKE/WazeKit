2e300085e8e701bac863d6909f1aa357e0b15020
brennanMKE <brennan@smallsharptools.com>
2017-03-20 18:38:19 -0700

    Initial Commit

---
+++ Waze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waze
+//
+//  Created by Brennan Stehling on 3/20/17.
+//  Copyright © 2017 SmallSharpTools LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
